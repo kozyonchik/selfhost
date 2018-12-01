717e30a60eb71090bb3d33c0f7d7443d4b839360
kozyonchik <DeeGS@yandex.ru>
2018-12-01 08:19:51 +0300

    1

--- untilda.js
+++ untilda.js
@@ -792,9 +792,9 @@ $(window).keypress(function(e) {
         $.each( achiver2, function( key, value ) {
 
             var $this = $(value[0])
-            console.log($this);
+            //console.log(value[0]);
             (!$this.hasClass('helpblock')?$this.addClass('hypeblock'):'');
-            $this.prepend('<div class="customcontrol"><div class="cc_but note tooltiper"><div class="th_out"><div class="th_in"><i class="fas fa-sticky-note"></i></div></div><div class="cc_noter">Название блока: '+value[1]+'<br><ol><li></li></ol></div></div><div class="cc_but preview"><div class="th_out"><div class="th_in"><i class="fas fa-eye"></i></div></div></div></div>')
+            $this.prepend('<div class="customcontrol special"><div class="cc_but note tooltiper"><div class="th_out"><div class="th_in"><i class="fas fa-sticky-note"></i></div></div><div class="cc_noter">Название блока: '+value[1]+'<br><ol><li></li></ol></div></div><div class="cc_but preview"><div class="th_out"><div class="th_in"><i class="fas fa-eye"></i></div></div></div></div>')
             /**/
         });
 
